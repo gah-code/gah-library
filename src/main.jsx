96048f5d51e19be7e8b3ac4eea962aebb7882467
Gilbert Haro <gilbertharocode@gmail.com>
2022-11-03 19:24:31 -0700

    Card Component

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './general.css';
 import './card.css';
+import './tabs.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
