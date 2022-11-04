3ad11e62b3b33c0204315d03528e3d9a62fa7a9d
Gilbert Haro <gilbertharocode@gmail.com>
2022-11-04 12:47:00 -0700

    buttons v1

---
+++ src/elements/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/buttons.css';
+
+const Button = () => {
+  return (
+    <div>
+      <a href='' class className='btn btn--full'>
+        Full
+      </a>
+
+      <a href='' className='btn btn--outline'>
+        Outline
+      </a>
+    </div>
+  );
+};
+
+export default Button;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './general.css';
-import './card.css';
+
 import './tabs.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
